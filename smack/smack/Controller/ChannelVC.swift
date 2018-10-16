93147ea70d6c8b130d0aaa6a5b723eb32df1ee76
Mario Galluscio <mgalluscio@deliverydudes.com>
2018-10-16 15:14:53 -0400

    UI updates

---
+++ smack/smack/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  smack
+//
+//  Created by Mario Galluscio on 10/14/18.
+//  Copyright © 2018 Mario Galluscio. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ smack/smack/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by Mario Galluscio on 10/14/18.
+//  Copyright © 2018 Mario Galluscio. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
