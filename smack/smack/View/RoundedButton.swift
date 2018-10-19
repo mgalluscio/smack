2e4917253cfdc99b8fa1f1436320c155fd6b3ac5
Mario Galluscio <mgalluscio@deliverydudes.com>
2018-10-18 21:56:06 -0400

    finished button view

--- smack/smack/View/RoundedButton.swift
+++ smack/smack/View/RoundedButton.swift
@@ -10,12 +10,16 @@ import UIKit
 
 @IBDesignable
 class RoundedButton: UIButton {
-    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
         }
     }
     override func awakeFromNib() {
+        self.setupView()
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
         self.setupView()
     }
     func setupView() {
